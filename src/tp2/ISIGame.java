642728ee9872b9531e1679ca660b634a30383dfe
Rania-Ben-Abdallah <74153166+Rania-Ben-Abdallah@users.noreply.github.com>
2021-11-19 20:50:03 +0100

    Update ISIGame.java

--- src/tp2/ISIGame.java
+++ src/tp2/ISIGame.java
@@ -3,18 +3,35 @@ package tp2;
 import java.util.Scanner;
 
 public class ISIGame {
-
-    static class ISIException extends RuntimeException{
+    
         public ISIException(String s) {
             super(s);
         }
+
+        public ISIException() {
+
+        }
     }
+
 
     public static void main(String[] args) {
         System.out.println("Enter a word");
 
         Scanner scanner = new Scanner(System.in);
         String line = scanner.nextLine();
+        line.toUpperCase();
+        try
+        {
+            if(line.contains("ISI"))
+            {
+                throw new ISIException();
+            }
+            System.out.println("OK");
+        }
+        catch(ISIException ex)
+        {
+            System.out.println("Life is not isi");
+        }
 
         // now you have to check if the word contains 'isi' or not
         // if not print 'ok'
