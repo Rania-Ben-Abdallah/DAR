fd50414350a096a6a527c0acf63d59e8d1052555
Rania-Ben-Abdallah <74153166+Rania-Ben-Abdallah@users.noreply.github.com>
2021-11-19 20:54:32 +0100

    Update Main.java

--- src/tp2/magicFile/Main.java
+++ src/tp2/magicFile/Main.java
@@ -1,38 +1,43 @@
 package tp2.magicFile;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
-
-    //Now we want to create a program that takes a file name and prints it s content
-
+    
     public static void main(String[] args) {
-        System.out.println("Give me your file name :)"); //try isi as an input ;)
-        Scanner scanner = new Scanner(System.in);
-        String fileName = scanner.nextLine();
-        printFileContent(fileName);
-    }
-
-    //Here you have 2 methods
-    // getFileContent aims to get the content of a file
     // printFileContent that prints it s content otherwise print 'file not found :/'
     // todo : implement the methods and force exception handling in the printFile method
 
     private static String getFileContent(String fileName) {
-        //use the line below if you're a linux user
-        //File file = new File("src/tp2/magicFile/" + fileName);
-
-        //use the line below if you're a windows user
-        File file = new File("src\\tp2\\magicFile\\" + fileName);
-
+        File file = new File("src/tp2/magicFile/" + fileName);
+    }
+    private static String getFileContent(String fileName) throws FileNotFoundException {
+        File file = new File("src/" + fileName);
         //the lines below help you to get the content
 //        Scanner reader = new Scanner(file);
 //        String content = reader.nextLine();
         return null;
+        Scanner reader = new Scanner(file);
+        String content = reader.nextLine();
+        return content;
     }
 
     private static void printFileContent(String fileName) {
+        String data = getFileContent(fileName);
+        System.out.println(data);
+        String data = null;
+        try {
+            data = getFileContent(fileName);
+            System.out.println(data);
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+
+
+    }
+}
         String data = getFileContent(fileName);
         System.out.println(data);
     }
