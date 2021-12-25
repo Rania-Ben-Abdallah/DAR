bdbd0d1aa6465675a88bc2348ba8e2e84f2b4693
Rania-Ben-Abdallah <74153166+Rania-Ben-Abdallah@users.noreply.github.com>
2021-12-25 23:13:20 +0100

    Update Server.java

--- src/tp4/Server.java
+++ src/tp4/Server.java
@@ -3,15 +3,23 @@ package tp4;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Locale;
 
 public class Server {
 
     static final int port = 1200;
-
+    public static String palidrome (String ch){
+        int n=ch.length();
+        for(int j=0;j<n/2;j++){
+            if(ch.charAt(j) != ch.charAt(n-j-1)){
+                return "false";
+            }
+        }
+        return "true";
+    }
     public static void main(String[] args) throws Exception {
 
-        // Listen to a specific port
-
+        // Listen to a specific p
         ServerSocket serverSocket = new ServerSocket(port);
         System.out.println("Waiting for connection");
         Socket socClient = serverSocket.accept(); // Accept a client socket
@@ -20,24 +28,22 @@ public class Server {
         // Initialize in / out
         BufferedReader inServer = new BufferedReader(new InputStreamReader(socClient.getInputStream()));
         PrintWriter outServer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socClient.getOutputStream())), true);
-
+        
+        // Activité1
         // Read message sent by the client
-        inServer.readLine();
+        String a=inServer.readLine();
         // Write message to the client
-        System.out.println("something");
-
-        // ****** if you're using DataInputStream & DataOutputStream ******
-
-        //DataInputStream inServer = new DataInputStream(socClient.getInputStream());
-        //DataOutputStream outServer = new DataOutputStream(socClient.getOutputStream());
-
-        // Read message sent by the client
-        //String s = inServer.readUTF();
-        //String strUpper = str.toUpperCase();
-        //System.out.println("The msg was " + strUpper);
-
-        // ****** if you're using DataInputStream & DataOutputStream ******
-
+        outServer.println(a.toUpperCase(Locale.ROOT));
+        
+        // Activité2
+        String x=inServer.readLine();
+        String y=inServer.readLine();
+        outServer.println(x.indexOf(y));
+        
+        // Activité3
+        String b=inServer.readLine();
+        outServer.println(palidrome(b));
+        
         // Close in / out
         inServer.close();
         outServer.close();
